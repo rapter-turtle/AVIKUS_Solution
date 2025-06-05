b754ecba0f0990297e67813fd90ee49f7a9919a5
lee-ck <leeck@kaist.ac.kr>
2025-06-05 12:33:32 +0900

    update LCK

---
+++ AVIKUS_SIM_Dock_Vanilla/NMPC/initialize_casadi_cmd.m
@@ -0,0 +1,184 @@
+function obj = initialize_casadi_cmd(Num,dt,Q,R,QN)
+import casadi.*
+USV_P = load_parm();
+%% Parameter setting - States
+x = SX.sym('x'); 
+y = SX.sym('y');
+psi = SX.sym('psi');
+u = SX.sym('u');
+v = SX.sym('v');
+r = SX.sym('r');
+TP = SX.sym('TP'); % Cmd 
+TS = SX.sym('TS'); % Cmd
+delPR = SX.sym('delPR');
+delSR = SX.sym('delSR'); 
+states = [x;y;psi;u;v;r;TP;TS;delPR;delSR]; 
+n_states = length(states);
+
+%% Parameter setting - Inputs
+d_TP = SX.sym('d_TP'); 
+d_TS = SX.sym('d_TS'); 
+d_delPR = SX.sym('d_delPR');
+d_delSR = SX.sym('d_delSR'); 
+controls = [d_TP;d_TS;d_delPR;d_delSR]; 
+n_controls = length(controls);
+
+
+%% Casadi setting   
+U = SX.sym('U',n_controls,Num); 
+P = SX.sym('P',n_controls+n_states + n_states *(Num+1));
+X = SX.sym('X',n_states,(Num+1));
+
+objective = 0; % Objective function
+g = [];  % constraints vector
+
+st  = X(:,1); % initial state
+g = [g;st-P(1:n_states)]; % initial condition constraints
+
+
+%% calculate objective function
+for k = 1:Num
+    st = X(:,k);  
+    con = U(:,k); 
+    ref_state = P(n_controls+n_states*k + 1:n_controls+n_states*k + n_states);   
+    objective = objective + (st-ref_state)'*Q*(st-ref_state);
+
+    %% USV dynamics
+    st_next = X(:,k+1);
+    objective = objective + con'*R*con;
+
+    xdot = usv_dynamics_mpc_cmd(st, con);
+    st_next_euler = st + (dt*xdot);
+    g = [g;st_next-st_next_euler]; % compute constraints
+
+    
+    s = 25;
+    kk = 8;
+    a1 = USV_P.KPrvs * USV_P.dead_rps;
+    a2 = USV_P.KPfwd * USV_P.dead_rps;
+    b1 = USV_P.KPrvs;
+    b2 = USV_P.KPfwd;
+
+    TP_prev = (1/(1+exp(s*X(7,k))))*(b1*X(7,k) + tanh(kk*X(7,k))*a1) + ...
+        (1/(1+exp(-s*X(7,k))))*(b2*X(7,k) + tanh(kk*X(7,k))*a2);
+    TS_prev = (1/(1+exp(s*X(8,k))))*(b1*X(8,k) + tanh(kk*X(8,k))*a1) + ...
+        (1/(1+exp(-s*X(8,k))))*(b2*X(8,k) + tanh(kk*X(8,k))*a2);
+    
+    TP_cur = (1/(1+exp(s*X(7,k+1))))*(b1*X(7,k+1) + tanh(kk*X(7,k+1))*a1) + ...
+        (1/(1+exp(-s*X(7,k+1))))*(b2*X(7,k+1) + tanh(kk*X(7,k+1))*a2);
+    TS_cur = (1/(1+exp(s*X(8,k+1))))*(b1*X(8,k+1) + tanh(kk*X(8,k+1))*a1) + ...
+        (1/(1+exp(-s*X(8,k+1))))*(b2*X(8,k+1) + tanh(kk*X(8,k+1))*a2);
+    
+    Q_d = 0.01;
+    objective = objective + (TP_cur-TP_prev)'*Q_d*(TP_cur-TP_prev);
+    objective = objective + (TS_cur-TS_prev)'*Q_d*(TS_cur-TS_prev);
+
+    
+    %% Dead zone
+    % param_s = 7;
+    % param_m = 10;
+    % 
+    % lower_bound = -USV_P.dead_rps * USV_P.KPrvs;
+    % upper_bound =  USV_P.dead_rps * USV_P.KPfwd;
+    % sig_lo_7 = 1 / (1 + exp(-param_s*(X(7,k) - lower_bound)));
+    % sig_hi_7 = 1 / (1 + exp(-param_s*(upper_bound - X(7,k))));
+    % sig_lo_8 = 1 / (1 + exp(-param_s*(X(8,k) - lower_bound)));
+    % sig_hi_8 = 1 / (1 + exp(-param_s*(upper_bound - X(8,k))));
+
+    % objective = objective + param_m * (sig_lo_7 * sig_hi_7);
+    % objective = objective + param_m * (sig_lo_8 * sig_hi_8);
+    
+end
+
+st = X(:,Num+1);  
+ref_state = P(n_controls+n_states*(Num+1) + 1: n_controls+n_states*(Num+1) + n_states);   
+objective = objective + (st-ref_state)'*QN*(st-ref_state);
+
+OPT_variables = [reshape(X,n_states*(Num+1),1);reshape(U,n_controls*Num,1)];
+nlp_prob = struct('f', objective, 'x', OPT_variables, 'g', g, 'p', P);
+
+opts = struct;
+opts.ipopt.max_iter = 20;
+opts.ipopt.print_level = 0; % 0 ~ 3
+opts.print_time = 0;
+opts.ipopt.acceptable_tol = 1e-8;
+opts.ipopt.acceptable_obj_change_tol = 1e-8;
+
+obj.solver = nlpsol('solver', 'ipopt', nlp_prob,opts);
+n_params = 1;
+obj.nx = n_states;
+obj.nu = n_controls;
+obj.np = n_params;
+
+%% Equaility contraints : Dyanmic equations 
+obj.args.lbg(1:n_states*(Num+1)) = -1e-10;  % -1e-20   % Equality constraints
+obj.args.ubg(1:n_states*(Num+1)) =  1e-10;  %  1e-20   % Equality constraints
+
+
+xmin = -1000;
+xmax = 13;
+ymin = -1;
+ymax = 3;
+psimin = -pi;
+psimax = pi;
+umin = -1.5;
+umax = 1.5;
+vmin = -1.5;
+vmax = 1.5;
+rmin = -1;
+rmax = 1;
+TPmin = -USV_P.ThrMax;
+TPmax = USV_P.ThrMax;
+TSmin = -USV_P.ThrMax;
+TSmax = USV_P.ThrMax;
+delPRmin = -USV_P.DelMax;
+delPRmax = USV_P.DelMax;
+delSRmin = -USV_P.DelMax;
+delSRmax = USV_P.DelMax;
+
+d_TPmin = -USV_P.ThrRate;
+d_TPmax = USV_P.ThrRate;
+d_TSmin = -USV_P.ThrRate;
+d_TSmax = USV_P.ThrRate;
+d_delPRmin = -USV_P.DelRate*0.5;
+d_delPRmax = USV_P.DelRate*0.5;
+d_delSRmin = -USV_P.DelRate*0.5;
+d_delSRmax = USV_P.DelRate*0.5;
+
+%% Inequaility contraints : States & Inputs
+obj.args.lbx(1:n_states:n_states*(Num+1),1) =  xmin;
+obj.args.ubx(1:n_states:n_states*(Num+1),1) =  xmax;
+obj.args.lbx(2:n_states:n_states*(Num+1),1) =  ymin;
+obj.args.ubx(2:n_states:n_states*(Num+1),1) =  ymax;
+obj.args.lbx(3:n_states:n_states*(Num+1),1) =  psimin;
+obj.args.ubx(3:n_states:n_states*(Num+1),1) =  psimax;
+obj.args.lbx(4:n_states:n_states*(Num+1),1) =  umin;
+obj.args.ubx(4:n_states:n_states*(Num+1),1) =  umax;
+obj.args.lbx(5:n_states:n_states*(Num+1),1) =  vmin;
+obj.args.ubx(5:n_states:n_states*(Num+1),1) =  vmax;
+obj.args.lbx(6:n_states:n_states*(Num+1),1) =  rmin;
+obj.args.ubx(6:n_states:n_states*(Num+1),1) =  rmax;
+obj.args.lbx(7:n_states:n_states*(Num+1),1) =  TPmin;
+obj.args.ubx(7:n_states:n_states*(Num+1),1) =  TPmax;
+obj.args.lbx(8:n_states:n_states*(Num+1),1) =  TSmin;
+obj.args.ubx(8:n_states:n_states*(Num+1),1) =  TSmax;
+obj.args.lbx(9:n_states:n_states*(Num+1),1) =  delPRmin;
+obj.args.ubx(9:n_states:n_states*(Num+1),1) =  delPRmax;
+obj.args.lbx(10:n_states:n_states*(Num+1),1) =  delSRmin;
+obj.args.ubx(10:n_states:n_states*(Num+1),1) =  delSRmax;
+
+
+obj.args.lbx(n_states*(Num+1)+1:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_TPmin;
+obj.args.ubx(n_states*(Num+1)+1:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_TPmax;
+obj.args.lbx(n_states*(Num+1)+2:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_TSmin;
+obj.args.ubx(n_states*(Num+1)+2:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_TSmax;
+obj.args.lbx(n_states*(Num+1)+3:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_delPRmin;
+obj.args.ubx(n_states*(Num+1)+3:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_delPRmax;
+obj.args.lbx(n_states*(Num+1)+4:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_delSRmin;
+obj.args.ubx(n_states*(Num+1)+4:n_controls:n_states*(Num+1)+n_controls*Num,1) = d_delSRmax;
+
+disp('*************** MPC setting ***************')
+disp(strcat('Prediction Num :` ' , num2str(Num)))
+disp(strcat('Prediction Ts :` ' , num2str(dt) ,'sec'))
+disp('Integration algorithm : Euler method')
+end
