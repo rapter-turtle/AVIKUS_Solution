81303f451a72af10d7e180422e8ff1041190a321
rapter-turtle <58213518+rapter-turtle@users.noreply.github.com>
2025-06-12 09:02:10 +0900

    Delete AVIKUS_SIM_Single_engine_Bow_Vanilla directory

--- AVIKUS_SIM_Single_engine_Bow_Vanilla/utils/Bound2Pi.m
+++
@@ -1,9 +0,0 @@
-function radian_new = Bound2Pi(radian)
-    if(radian < -pi)
-        radian_new = radian + 2 * pi;
-    elseif (radian > pi)
-        radian_new = radian - 2 * pi;
-    else
-        radian_new = radian;
-    end
-end
